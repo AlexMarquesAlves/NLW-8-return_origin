80ae0945083e73af45784da69aa72fd201e9b7c2
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-05 23:19:12 -0300

    Feat: added new elements on ScrollReveal

--- main.js
+++ main.js
@@ -22,17 +22,20 @@ function closeMenu() {
   document.body.classList.remove(`menu-expanded`);
 }
 
-ScrollReveal({ origin: `top`, distance: `30px`, duration: 700 }).reveal(`
-#home,
-#home img,
-#home .stats,
-#services,
-#services header,
-#services .card,
-#about,
-#about header,
-#about .content,
-`);
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+  #home,
+  #home img,
+  #home .stats,
+  #services,
+  #services header,
+  #services .card
+  #about,
+  #about header,
+  #about .content`);
 
 function showNavOnScroll() {
   if (scrollY > 0) {
