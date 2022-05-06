42730cf6ca812078ae616b2ee7caa95e044417eb
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-05-05 23:18:38 -0300

    Feat: new functions on scroll

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+window.addEventListener("scroll", onScroll);
+
+onScroll();
+function onScroll() {
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
+}
 function onScroll() {
   if (scrollY > 0) {
     navigation.classList.add(`scroll`);
@@ -26,3 +33,19 @@ ScrollReveal({ origin: `top`, distance: `30px`, duration: 700 }).reveal(`
 #about header,
 #about .content,
 `);
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
